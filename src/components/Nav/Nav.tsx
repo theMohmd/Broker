550a5130b21549fd3d88e65154629bdf97f845e0
Mohmd <mim.mohrechi@gmail.com>
2024-04-05 21:45:06 +0330

    changed nav height

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,7 +3,7 @@ import NavButton from "./NavButton";
 
 const Nav = () => {
     return (
-        <div className="grid border-b h-[60px] md:h-[80px]">
+        <div className="grid border-b h-[40px] md:h-[60px]">
             <div dir="rtl" className="container md:gap-2 flex items-center">
                 <NavLink to="/" className="flex justify-center items-center ml-auto text-xl font-bold md:text-4xl text-primary">
                     املاک
