deb521bd54ad1c42ccd80dc3a667b4bd41f98a3f
Mohmd <mim.mohrechi@gmail.com>
2024-04-05 21:37:15 +0330

    added pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Nav from "components/Nav/Nav";
 import { Route, Routes } from "react-router-dom";
-
+import Home from "pages/Home";
+import Login from "pages/Login";
+import Post from "pages/Post";
 const App = () => {
     return (
         <div>
             <Nav />
             <Routes>
-                <Route path="/" element={<div>hi</div>} />
-                <Route path="/login" element={<div>login</div>} />
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/post" element={<Post />} />
             </Routes>
         </div>
     );
